a2a53e7739d7329c536610fee9fc216fb5c15167
unknown <andrew.a.ruiz92@gmail.com>
2017-09-07 21:41:04 -0400

    hw

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('The Child Molester');
+INSERT INTO burger (name) VALUES ('Foot Feta-ish Burger');
+INSERT INTO burger (name, devoured) VALUES ('Grandpa Muenster Burger', true);
+INSERT INTO burger (name, devoured) VALUES ('Bohemian Radishy Burger', true);
+INSERT INTO burger (name, devoured) VALUES ('Kales From the Crypt Burger', true);
+INSERT INTO burger (name) VALUES ('Paranormal Pepper Jack-tivity Burger');
